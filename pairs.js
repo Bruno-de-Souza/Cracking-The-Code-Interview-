5b11cb64ad7f4c044d807a044a03a92b8109a486
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-01 23:56:22 -0300

    Add pairs function to generate all possible pairs in pairs.js

---
+++ pairs.js
@@ -0,0 +1,10 @@
+function pairs() {
+    n = ['1', '2', '3', '4'];
+    for (let i = 0; i < n.length; i++) {
+        for (let j = 0; j < n.length; j++) {
+            console.log(n[i] + ',' + n[j]);
+        }
+    }
+    console.log('\n=======================================================\n')
+}
+console.log(pairs());
